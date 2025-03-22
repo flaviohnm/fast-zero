693f12f1654d4ac61a11dec29c910c544ba36a18
Flavio Monteiro <flaviohnm@gmail.com>
2025-03-21 23:26:30 -0300

    :construction: feat: add schema to project

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,9 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app) # Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') # Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
