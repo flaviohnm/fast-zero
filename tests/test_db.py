65e93702d3a0c73aece38c2e93fac1700db10e9f
Flavio Monteiro <flaviohnm@gmail.com>
2025-04-06 22:41:51 -0300

    :construction: init: add database connection on project

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,10 +7,11 @@ from fast_zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(username='alice', password='secret', email='test@test')
-
-    session.add(new_user)
-    session.commit()
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
+        session.add(new_user)
+        session.commit()
 
     user = session.scalar(select(User).where(User.username == 'alice'))
 
@@ -18,7 +19,7 @@ def test_create_user(session, mock_db_time):
         'id': 1,
         'username': 'alice',
         'password': 'secret',
-        'email': 'test@test',
+        'email': 'teste@test',
         'created_at': time,
-        'updated_at': time,
+        'updated_at': time,  # Exercício
     }
