e5d3d2ef5ae3cf94c374a830b3158d17c355f166
Flavio Monteiro <flaviohnm@gmail.com>
2025-04-14 08:56:36 -0300

    :recycle: refactor: created routers for User and Auth - moved constants to environment variables

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -11,3 +11,24 @@ def test_get_token(client, user):
     assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert 'token_type' in token
+
+
+def test_get_token_with_not_found_user(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': 'Alice', 'password': user.clean_password},
+    )
+    # token = response.json()
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_get_token_with_wrong_password(client, user):
+    response = client.post(
+        '/auth/token', data={'username': user.email, 'password': 'test123'}
+    )
+    # token = response.json()
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email or password'}
